fbf3459b2ac546044a4e364851b846b7823e59be
Captain Storm <CaptainStorm21@users.noreply.github.com>
2025-02-09 23:36:43 -0800

    set up the main tree

--- main.dart
+++ main.dart
@@ -1,11 +1,14 @@
-import 'finalconst.dart';
+import 'main2.dart';
 import './first_part/first.dart';
 import './first_part/third.dart';
 import './first_part/second.dart.';
+import './first_part/finalconst.dart';
 
 void main(List<String> args) {
+  // Part 1
   firstMain(args);
   secondMain(args);
   thirdMain(args);
   finalConstMain(args);
+  main2(args);
 }
---
+++ main2.dart
@@ -0,0 +1,13 @@
+import './second_part/if_else.dart';
+import 'second_part/if_else_elseif.dart';
+
+void main() {
+  // Call main2 directly when running main2.dart
+  main2([]);
+}
+
+void main2(List<String> args) {
+  // Part 2
+  ifElseMain(args);
+  ifElseElseIfMain(args);
+}

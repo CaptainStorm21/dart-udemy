d9caeb7ea2c995cdaf62af21b687807cb01e85dc
Captain Storm <CaptainStorm21@users.noreply.github.com>
2025-02-10 00:00:53 -0800

    added 1 more condition

--- second_part/if_else_elseif.dart
+++ second_part/if_else_elseif.dart
@@ -2,11 +2,22 @@ void ifElseElseIfMain(List<String> args) {
   // Part 2
   int num1 = 5;
   int num2 = 15;
+  int num3 = 35;
 
-  if (num1 < num2) {
+if (num1 < num2) {
     print('$num1 is smaller than $num2');
-  } else if (num1 > num2) {
-    print('$num1 is greater than $num2');
+  }
+
+  if (num1 < num3) {
+    print('$num1 is smaller than $num3');
+  }
+
+  if (num3 != 0) {
+    print('num3 is not equal to 0');
+  }
+
+  if (num1 >= num2 && num1 >= num3) {
+    print('$num1 is greater than or equal to both $num2 and $num3');
   } else {
     print('$num1 is equal to $num2');
   }
